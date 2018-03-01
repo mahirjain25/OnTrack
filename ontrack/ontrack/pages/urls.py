2f2c9576c188bae19698b6a1d3858428fe607f9d
suraj44 <suraj1998@gmail.com>
2018-03-01 15:01:25 +0530

    created initial UI

---
+++ ontrack/ontrack/pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('', views.homePageView.as_view(),name = 'home'),
+	path('about', views.AboutPageView.as_view(),name = 'about'),
+]
---
+++ ontrack/ontrack/pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+# Create your views here.
+
+class homePageView(TemplateView):
+	template_name = "home.html"
+	
+
+class AboutPageView(TemplateView):
+	template_name = "about.html"
