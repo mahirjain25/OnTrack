a5d6c65672f98dbbde8eeecaf74c3d54aa93f5db
mahirjain25 <mahirjain25@gmail.com>
2018-03-06 22:02:11 +0530

    Created model for inventory

--- ontrack/ontrack/inventory/models.py
+++ ontrack/ontrack/inventory/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
 # Create your models here.
+
+class Clothes(models.Model):
+    TEES = 'TEE'
+    PANTS = 'PNT'
+    JEANS = 'JNS'
+    SHIRTS = 'SHR'
+    SHORTS = 'SHT'
+    UNDERGARMENTS = 'UNG'
+    SOCKS = 'SCK'
+    ETHNICTOP = 'ETT'
+    ETHNICBOTTOM = 'ETB'
+    TOP = 'TOP'
+    SKIRT = 'SKT'
+    LEGGINGS = 'LEG'
+    DRESS = 'DRS'
+    
+    FORMAL = 'FL'
+    CASUAL = 'CL'
+    SEMIFORMAL = 'SF'
+    NIGHTWEAR = 'NW'
+    
+    TYPE_CHOICES = (
+    	(TEES,'Tees'),
+    	(PANTS,'Pants'),
+    	(JEANS,'Jeans'),
+    	(SHIRTS,'Shirts'),
+    	(SHORTS,'Shorts'),
+    	(UNDERGARMENTS,'Undergarments'),
+    	(SOCKS,'Socks'),
+    	(ETHNICTOP,'Ethnic Top'),
+    	(ETHNICBOTTOM,'Ethnic Bottom'),
+    	(TOP,'Women\'s Top'),
+    	(SKIRT,'Skirt'),
+    	(LEGGINGS,'Leggings/Skins'),
+    	(DRESS,'Women\'s Dress'),
+    )
+    
+    CATEGORY_CHOICES = (
+    (FORMAL,'Formal Wear'),
+    (CASUAL, 'Casual Wear'),
+    (SEMIFORMAL, 'Semi-Formal Wear'),
+    (NIGHTWEAR,'Night Wear'),
+    )
+    
+    COLOUR_CHOICES = (
+	('WT','White'),    
+    ('RD','Red'),
+    ('BL','Black'),
+    ('BE','Blue'),
+    ('BR','Brown'),
+    ('YL','Yellow'),
+    ('OR','Orange'),
+    ('PL','Purple'),
+    ('GR','Green'),
+    ('GY','Grey'),
+    ('PK','Pink'),
+    ('OT','Other'),
+    )
+    quantity = models.IntegerField(db_column = 'Qty')
+    types = models.CharField(choices=TYPE_CHOICES,max_length=3,blank=False,db_column = 'Type')
+    category = models.CharField(choices=CATEGORY_CHOICES,max_length=2,blank=False,db_column = 'Category')
+    colour = models.CharField(choices=COLOUR_CHOICES,max_length=2,db_column = 'Colour')
+    tag = models.TextField()
+    
+    
+    
