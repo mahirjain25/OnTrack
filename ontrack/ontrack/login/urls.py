cbd4e97ca433c88bbda182ad89db964389e3feaa
Manish Sridhar <admin@MANISH.MANISH>
2018-03-15 14:29:19 +0530

    changed signup

--- ontrack/ontrack/login/urls.py
+++ ontrack/ontrack/login/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.login_user),
-    path('signup/', views.signup_user)
+    path('signup/', views.SignUp.as_view())
 
     
 ]
